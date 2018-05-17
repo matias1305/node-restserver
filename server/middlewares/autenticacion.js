ff62f06f4c5bb3e15d49ece6702faa3257b0a710
Matias-Espinoza <mespinoza@ubicomp-labs.com>
2018-05-16 21:51:48 -0400

    Login final

--- server/middlewares/autenticacion.js
+++ server/middlewares/autenticacion.js
@@ -4,42 +4,43 @@ const jwt = require('jsonwebtoken');
 // Verificar token
 // =====================
 let verificaToken = (req, res, next) => {
-   let token = req.get('Authorization');
+    let token = req.get('Authorization');
 
-   jwt.verify( token, process.env.SEDD, (err, decoded) => {
-      if(err){
-         return res.status(401).json({
-            ok: false,
-            err: {
-               message: 'Authorization invalid'
-            }
-         });
-      }
+    jwt.verify(token, process.env.SEDD, (err, decoded) => {
+        if (err) {
+            return res.status(401).json({
+                ok: false,
+                err: {
+                    message: 'Authorization invalid'
+                }
+            });
+        }
 
-      req.usuario = decoded.usuario;
-   });
-   next();
+        req.usuario = decoded.usuario;
+    });
+    next();
 };
 
 // =====================
 // Verificar AdminRole
 // =====================
 let verificaAdmin_Role = (req, res, next) => {
-   let usuario = req.usuario;
+    let usuario = req.usuario;
 
-   if( usuario.role === 'ADMIN_ROLE' ){
-      next();
-   }else {
-      return res.json({
-         ok: false,
-         err: {
-            message: 'El usuario no es administrador'
-         }
-      });
-   }
+    if (usuario.role === 'ADMIN_ROLE') {
+        next();
+    } else {
+        return res.json({
+            ok: false,
+            err: {
+                message: 'El usuario no es administrador'
+            }
+        });
+        next();
+    }
 };
 
 module.exports = {
-   verificaToken,
-   verificaAdmin_Role
+    verificaToken,
+    verificaAdmin_Role
 };
--- server/routes/usuario.routes.js
+++ server/routes/usuario.routes.js
@@ -11,120 +11,118 @@ const app = express();
 // Peticion para obtener los usuarios de la bd.
 app.get('/usuario', verificaToken, (req, res) => {
 
-   let desde = req.query.desde || 0;
-   desde = Number(desde);
+    let desde = req.query.desde || 0;
+    desde = Number(desde);
 
-   let limite = req.query.limite || 5;
-   limite = Number(limite);
+    let limite = req.query.limite || 5;
+    limite = Number(limite);
 
-   Usuario.find({ estado: true }, 'nombre email role img estado google')
-            .skip(desde)
-            .limit(limite)
-            .exec( (err, usuarios) => {
-               if( err ){ // Status 400 en caso de fallar.
-                  return res.status(400).json({
-                     ok: false,
-                     err: err
-                  });
-               }
+    Usuario.find({ estado: true }, 'nombre email role img estado google')
+        .skip(desde)
+        .limit(limite)
+        .exec((err, usuarios) => {
+            if (err) { // Status 400 en caso de fallar.
+                return res.status(400).json({
+                    ok: false,
+                    err: err
+                });
+            }
 
-               Usuario.count({ estado: true }, (err, conteo) => {
-                  res.json({
-                     ok: true,
-                     usuarios,
-                     cuantos: conteo
-                  });
-               });
+            Usuario.count({ estado: true }, (err, conteo) => {
+                res.json({
+                    ok: true,
+                    usuarios,
+                    cuantos: conteo
+                });
             });
-
-
+        });
 });
 
 // =================================================
 // Solicitud post para agregar a la bd.
 app.post('/usuario', [verificaToken, verificaAdmin_Role], (req, res) => {
-   let body = req.body;
+    let body = req.body;
 
-   // Obteniendo la data
-   let usuario = new Usuario({
-      nombre: body.nombre,
-      email: body.email,
-      password: bcrypt.hashSync(body.password, 10),
-      role: body.role
-   });
+    // Obteniendo la data
+    let usuario = new Usuario({
+        nombre: body.nombre,
+        email: body.email,
+        password: bcrypt.hashSync(body.password, 10),
+        role: body.role
+    });
 
-   // Guardando en mongoDB
-   usuario.save( (err, usuarioDB) => {
-      if( err ){ // Status 400 en caso de fallar.
-         return res.status(400).json({
-            ok: false,
-            err: err
-         });
-      }
+    // Guardando en mongoDB
+    usuario.save((err, usuarioDB) => {
+        if (err) { // Status 400 en caso de fallar.
+            return res.status(400).json({
+                ok: false,
+                err: err
+            });
+        }
 
-      res.json({ // Status 200 en caso de exito.
-         ok: true,
-         usuario: usuarioDB
-      });
-   });
+        res.json({ // Status 200 en caso de exito.
+            ok: true,
+            usuario: usuarioDB
+        });
+    });
 });
 
 
 // ==================================================
 // Solicitud put para actualizar a la bd.
-app.put('/usuario/:id', [verificaToken, verificaAdmin_Role], (req, res) => {
-   let id = req.params.id;
-   // Solo envio los datos que pueden ser modificados.
-   let body = _.pick(req.body, ['nombre','email','img','role','estado']);
+app.put('/usuario/:id', verificaToken, (req, res) => {
+    let id = req.params.id;
 
-   Usuario.findByIdAndUpdate( id, body, {new: true, runValidators: true}, (err, usuarioDB) => {
-      if( err ){ // Status 400 en caso de fallar.
-         return res.status(400).json({
-            ok: false,
-            err: err
-         });
-      }
+    // Solo envio los datos que pueden ser modificados.
+    let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']);
+    Usuario.findByIdAndUpdate(id, body, { new: true }, (err, usuarioDB) => {
+        console.log(usuarioDB);
+        if (err) { // Status 400 en caso de fallar.
+            return res.status(400).json({
+                ok: false,
+                err: err
+            });
+        }
 
-      res.json({
-         ok: true,
-         usuario: usuarioDB
-      });
-   });
-
+        res.json({
+            ok: true,
+            usuario: usuarioDB
+        });
+    });
 });
 
 
 // =================================================
 // Solicitud delete para borrar un registro a la bd
 app.delete('/usuario/:id', [verificaToken, verificaAdmin_Role], (req, res) => {
-   let id = req.params.id;
+    let id = req.params.id;
 
-   let cambiaEstado = {
-      estado: false
-   }
+    let cambiaEstado = {
+        estado: false
+    }
 
-   Usuario.findByIdAndUpdate( id, cambiaEstado, {new: true}, (err, usuarioBorrado) => {
-      if( err ){ // Status 400 en caso de fallar.
-         return res.status(400).json({
-            ok: false,
-            err: err
-         });
-      }
+    Usuario.findByIdAndUpdate(id, cambiaEstado, { new: true }, (err, usuarioBorrado) => {
+        if (err) { // Status 400 en caso de fallar.
+            return res.status(400).json({
+                ok: false,
+                err: err
+            });
+        }
 
-      if( !usuarioBorrado ){
-         return res.status(400).json({
-            ok: false,
-            err: {
-               message: 'Usuario no econtrado'
-            }
-         });
-      }
+        if (!usuarioBorrado) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: 'Usuario no econtrado'
+                }
+            });
+        }
 
-      res.json({
-         ok: true,
-         usuario: usuarioBorrado
-      });
-   });
+        res.json({
+            ok: true,
+            usuario: usuarioBorrado
+        });
+    });
 });
 
 
